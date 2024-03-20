4e24577a4b8c2c8db7fe4fe784c107dd76b097a9
Dragonlele2000 <143609841+Dragonlele2000@users.noreply.github.com>
2024-03-20 23:13:52 +0800

    Add files via upload

---
+++ GNN改三/GetDataset.py
@@ -0,0 +1,58 @@
+# 从文件获取数据集
+from config_test import *
+from VisualizeGraph import visualize_graph
+import random
+import torch
+from torch_geometric.data import Data
+
+
+def get_dataset_from_file():
+    file = open(file_name, 'r')
+    n = int(file.readline())
+    #修改
+    n = int(n * use_ratio)
+    # if n != num_instance:
+    #     print("Num_instance not consistent!")
+    dataset = []
+    for instance in range(n):
+        v, positive = file.readline().split(' ')
+        v, positive = int(v), int(positive)
+        e_i = [int(i) for i in file.readline().split(' ')]
+        e_j = [int(j) for j in file.readline().split(' ')]
+        v_color = [int(c) for c in file.readline().split(' ')]
+        y = [int(y_i) for y_i in file.readline().split(' ')]
+        # 改动改动改动改动改动改动改动改动改动改动改动改动
+        if change_cri:
+            for i in range(len(y)):
+                if y[i] > 1:
+                    y[i] = 1
+
+        x = [[] for _ in range(v)]
+        for v_i in range(v):
+            color = v_color[v_i]
+            x[v_i] = [0 for _ in range(k * (1 << k))]
+            x[v_i][color * (1 << k) + (1 << color)] = 1
+        edge_index = torch.tensor([e_i, e_j], dtype=torch.long)
+        x = torch.tensor(x, dtype=torch.float)
+        y = torch.tensor(y, dtype=torch.float)
+        data = Data(x=x, edge_index=edge_index, y=y)
+        dataset.append(data)
+
+    file.close()
+    return dataset
+
+
+def get_dataset():
+    dataset = get_dataset_from_file()
+
+    torch.manual_seed(randomSeed)
+    random.seed(randomSeed)
+    random.shuffle(dataset)
+
+    total_size = len(dataset)
+    train_size = int(total_size * train_ratio)
+
+    dataset = dataset[:total_size]
+    train_dataset = dataset[:train_size]
+    test_dataset = dataset[train_size:]
+    return train_dataset, test_dataset
---
+++ GNN改三/Ideal.py
@@ -0,0 +1,36 @@
+import numpy as np
+from VisualizeMatrix import visualize_matrix
+from config_test import k, change_cri
+
+def rel():
+    arr = np.zeros((k * (1 << k), (k * (1 << k))))
+
+    for color in range(k):
+        istart = color * (1 << k)
+        for i in range((1 << k)):
+            for j in range((1 << k)):
+                if (i >> color) % 2 == 1 and j == i - (1 << color) and j != 0:
+                    for j2 in range(k):
+                        arr[i + istart][j + (1 << k) * j2] = 1
+    visualize_matrix(arr, 'Analyze\\IdealRel-' + str(k) + '.txt')
+    return arr
+
+
+def root():
+    arr = np.zeros((k * (1 << k), (k * (1 << k)))) * (-1)
+    # arr = np.identity(k * (1 << k))
+    # print(arr)
+    for color in range(k):
+        j = color * (1 << k) + (1 << color)
+        arr[j][j] = 1
+        for i in range(k * (1 << k)):
+            if not (color * (1 << k) <= i < (color + 1) * (1 << k)):
+                arr[i][j] = -1e4
+
+    visualize_matrix(arr, 'Analyze\\IdealRoot-' + str(k) + '.txt')
+    return arr
+
+
+if __name__ == '__main__':
+    rel()
+    root()
